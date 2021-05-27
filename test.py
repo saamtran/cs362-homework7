5fd6c7eb243f50361578d11547cd8c2f1d5eb579
saamtran <71792958+saamtran@users.noreply.github.com>
2021-05-27 13:36:16 -0700

    added full implementation of the code

--- test.py
+++ test.py
@@ -3,13 +3,17 @@ def fizzbuzz():
     array = []
     for x in range(1, 101):
         if x % 3 == 0:
-            array.append("Fizz")
+            if x % 5 == 0:
+                array.append("Fizzbuzz")
+            
+            else:
+                array.append("Fizz")
         
         elif x % 5 == 0:
             array.append("buzz")
         
         else:
             array.append(x)
-    print(array)
+
     return array
 fizzbuzz()
