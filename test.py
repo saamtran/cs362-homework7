1121e3c61663387683a726da14efc23b785f19a7
saamtran <71792958+saamtran@users.noreply.github.com>
2021-05-27 13:34:42 -0700

    added buzz code, where every 5 it states buzz

--- test.py
+++ test.py
@@ -2,7 +2,14 @@
 def fizzbuzz():
     array = []
     for x in range(1, 101):
-        array.append(x)
-
+        if x % 3 == 0:
+            array.append("Fizz")
+        
+        elif x % 5 == 0:
+            array.append("buzz")
+        
+        else:
+            array.append(x)
+    print(array)
     return array
 fizzbuzz()
