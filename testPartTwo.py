4e436f12ca79beb27e0cd96bc9277dc949604a7b
saamtran <71792958+saamtran@users.noreply.github.com>
2021-05-27 13:53:48 -0700

    1900 shouldn't have been a leap year. we've fixed this issue!

--- testPartTwo.py
+++ testPartTwo.py
@@ -3,7 +3,7 @@ import partTwo
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        self.assertEqual(partTwo.mainFunction(2000), True)
+        self.assertEqual(partTwo.mainFunction(1900), True)
 
 if __name__ == '__main__':
     unittest.main()
