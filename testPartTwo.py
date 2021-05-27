3b1bfe6994ceb86dc66026f85442d325a0a46b79
saamtran <71792958+saamtran@users.noreply.github.com>
2021-05-27 13:56:47 -0700

    does not take in the fact that if it's divisible by 400.

--- testPartTwo.py
+++ testPartTwo.py
@@ -3,7 +3,17 @@ import partTwo
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        self.assertEqual(partTwo.mainFunction(1900), True)
+        self.assertEqual(partTwo.mainFunction(1900), False)
+
+
+class TestCase2(unittest.TestCase):
+    def test2(self):
+        self.assertEqual(partTwo.mainFunction(2020), True)
+
+class TestCase3(unittest.TestCase):
+    def test3(self):
+        self.assertEqual(partTwo.mainFunction(2017), False)
 
 if __name__ == '__main__':
     unittest.main()
+
